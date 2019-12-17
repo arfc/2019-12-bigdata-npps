4e3dfe06f9e85d3c0bdd422455eb283b2800e0d2
mehmeturkmen <53832969+mehmeturkmen@users.noreply.github.com>
2019-12-17 10:30:35 -0600

    Update TempIdentifier.py

--- raven/projects/Scripts/TempIdentifier.py
+++ raven/projects/Scripts/TempIdentifier.py
@@ -3,7 +3,7 @@ def evaluate(self):
 
     if self.Tave_fuel < 900:
        temp_identifier = '06'   
-    elif self.Tave_fuel < 1050:
+    elif self.Tave_fuel < 1200:
        temp_identifier = '09'
     else:
        temp_identifier = '12'
